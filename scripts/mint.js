1c3a6a5b5db666d78b45d8f44d442b830dd083cb
hobbescodes <jakob.hauble@gmail.com>
2022-06-30 10:15:58 -0500

    add mint script

---
+++ scripts/mint.js
@@ -0,0 +1,25 @@
+const { ethers, network } = require("hardhat")
+const { moveBlocks } = require("../utils/move-blocks")
+
+const PRICE = ethers.utils.parseEther("0.1")
+
+const mint = async () => {
+    const nftMarketplace = await ethers.getContract("NftMarketplace")
+    const basicNft = await ethers.getContract("BasicNft")
+    console.log("Minting...")
+    const mintTx = await basicNft.mintNft()
+    const txReceipt = await mintTx.wait(1)
+    const tokenId = txReceipt.events[0].args.tokenId
+    console.log(`Got Token Id: ${tokenId} from NFT Address: ${basicNft.address}`)
+
+    if ((network.config.chainId = "31337")) {
+        await moveBlocks(2, (sleepAmount = 1000))
+    }
+}
+
+mint()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
